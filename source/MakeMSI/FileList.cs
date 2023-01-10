07d9c163fd036cd255523c8a18774887fd58581a
MicroBlaster <dmc73120@gmail.com>
2023-01-10 15:35:14 -0600

    Updated Installer

---
+++ source/MakeMSI/FileList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MakeMSI
+{
+    public class FileList
+    {
+        public int DirIndex { get; private set; }
+        public int FileIndex { get; private set; }
+        public string Dirs { get; private set; }
+        public string Files  { get; private set; }
+        public string Root { get; private set; }
+
+        private StringBuilder db;
+        private StringBuilder fb;
+
+        public FileList(string root)
+        {
+            DirIndex = -1;
+            FileIndex = 0;
+            Dirs = "";
+            Files = "";
+            Root = root;
+
+            db = new();
+            fb = new();
+        
+            Enumerate();
+
+            Dirs = db.ToString();
+            Files = fb.ToString();
+        }
+
+        private void Enumerate(String path = "")
+        {
+
+            try
+            {
+                foreach (var d in System.IO.Directory.GetDirectories(@$"{Root}\{path}"))
+                {
+                    var dir = new DirectoryInfo(d);
+
+
+                    if (!((dir.Name == "MakeMSI") || (dir.Name == "MakeBot") || (dir.Name == "Installer")))
+                    {
+                        DirIndex++;
+                        Console.WriteLine(@$"D{DirIndex:D4}:{path}\{dir.Name}");
+
+                        db.AppendLine($"<Directory Id=\"D{DirIndex:D4}\" Name=\"{dir.Name}\">");
+
+                        int fileCount = 0;
+                        foreach (var f in Directory.GetFiles(@$"{Root}\{path}\{dir.Name}"))
+                        {
+                            var file = new FileInfo(f);
+                            if (((file.Extension == ".ts") || (file.Extension == ".wav") || (file.Extension == ".txt") ||
+                               (file.Extension == ".cts") && !File.Exists(file.FullName.Replace(".cts", ".ts"))) &&
+                               file.Name != "Directory Structure.txt")
+                            {
+                                fileCount++;
+                            }
+                        }
+
+                        if (fileCount > 0) {
+                            fb.AppendLine($"<Component Id=\"C{DirIndex:D4}\" Directory=\"D{DirIndex:D4}\" Guid=\"{Guid.NewGuid()}\">");
+                            foreach (var f in Directory.GetFiles(@$"{Root}\{path}\{dir.Name}"))
+                            {
+                                var file = new FileInfo(f);
+                                if (((file.Extension == ".ts") || (file.Extension == ".wav") || (file.Extension == ".txt") ||
+                                   (file.Extension == ".cts") && !File.Exists(file.FullName.Replace(".cts", ".ts"))) &&
+                                   file.Name != "Directory Structure.txt")
+                                {
+                                    //Console.WriteLine(@$"D{Index:D4}:{path}\{dir.Name}\{file.Name}");
+
+                                    //fb.AppendLine($"    <File Id=\"F{FileIndex:D4}\" Source=\"{file.FullName}\"/>");
+                                    fb.AppendLine($"    <File Id=\"F{FileIndex:D4}\" Source=\"..{path}\\{dir.Name}\\{file.Name}\"/>");
+                                    FileIndex++;
+                                }
+                            }
+                            fb.AppendLine($"</Component>");
+                        }
+
+                        // Recursive call to inumerate sub directories
+                        Enumerate(@$"{path}\{dir.Name}");
+
+                        db.AppendLine($"</Directory>");
+                    }
+                }
+
+
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- source/MakeMSI/Program.cs
+++ source/MakeMSI/Program.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using MakeMSI;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using static System.Net.Mime.MediaTypeNames;
+
+//candle and light command lines:
+//    -ext WixUIExtension - ext WixUtilExtension
 
 class Test
 {
     public static void Main()
     {
-        
         string path = Environment.CurrentDirectory;
         path = path.Replace(@"\bin\Debug\net7.0", "");
         path = path.Replace(@"\bin\Release\net7.0", "");
         path = path.Replace(@"\MakeMSI", "");
         string file = @$"{path}\Installer\Product.wxs";
+
+        FileList fl = new(path);
 
         using (StreamWriter sw = File.CreateText(file))
         {
@@ -35,8 +42,27 @@ class Test
                 		</Property>
                 		<Property Id="WIXUI_INSTALLDIR" Value="MOMBOT" />
                 		<UIRef Id="WixUI_InstallDir" />
-                   </Product>
-                   
+                  
+                <UI>
+                    
+                    <Publish Dialog="ExitDialog" 
+                        Control="Finish" 
+                        Event="DoAction" 
+                        Value="LaunchApplication">NOT Installed</Publish>
+                </UI>
+                <!--
+                <Property Id="WIXUI_EXITDIALOGOPTIONALCHECKBOXTEXT" Value="Compile Mind ()ver Matter bot now." />
+                  -->
+                <!-- Step 3: Include the custom action -->
+                <Property Id="WixShellExecTarget" Value="[#MakeBot.exe]" />
+                <CustomAction Id="LaunchApplication" 
+                    BinaryKey="WixCA" 
+                    DllEntry="WixShellExec"
+                    Impersonate="yes" />                        
+                </Product>
+                 
+
+
                    <Fragment>
                         <WixVariable Id="WixUISupportPerUser" Value="0" />
                         <Property Id="ApplicationFolderName" Value="TWXProxy\Scripts\Mombot" />
@@ -53,15 +79,13 @@ class Test
                                     <Directory Id="TWXPROXY" Name="TWXProxy" >
                                         <Directory Id="SCRIPTS" Name="Scripts" >
                                             <Directory Id="MOMBOT" Name="Mombot">
+                                                <Directory Id="Source" Name="Source" >
+                """);
 
-                                <Directory Id="Source" Name="Source" >
-
-                                </Directory>
-               
-
-                  <!--      <Component Id="ProductComponent">
-                            <RegistryValue Id="InstallPath" Root="HKCU" Key="Software\Xide\TWXP"  Name="Mombot" Type="string" Value="[Mombot]" />
-                        </Component>-->
+            sw.WriteLine(fl.Dirs);
+                
+            sw.WriteLine("""                
+                                                </Directory>
                                             </Directory>
                                         </Directory>
                                     </Directory>
@@ -69,27 +93,33 @@ class Test
                             </Directory>
                         </Directory>	   
 
-
                         <ComponentGroup Id="ProductComponents">
                             <Component Id="ProductComponent" Directory="Source" Guid="72a94083-4c7e-4365-aa2e-10d1aa6721c2"  >
                                <RegistryValue Id="InstallPath" Root="HKCU" Key="Software\Xide\TWXP"  Name="Mombot" Type="string" Value="[Mombot]" />
                                <File Source="..\mombot.ts"/>
+                               <File Source="..\Directory Structure.txt"/>
+                               <File Source="..\page.wav"/>
+                               <File Source="..\StripExample.ts"/>
+                               <File Source="..\MakeBot\bin\Debug\MakeBot.exe"/>                               
+                               <File Source="..\twxc.exe"/>                                          
                             </Component>
-                        </ComponentGroup>
-                                
+                """);
 
+            sw.WriteLine(fl.Files);
+
+            sw.WriteLine("""                
+                        </ComponentGroup>
                     </Fragment>
                 </Wix>                
-                """);
+                """
+            );
         }
+    //C:\Program Files(x86)\WiX Toolset v3.11\bin\candle.exe - dDebug - d"DevEnvDir=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\\" - dSolutionDir = C:\Projects\TW2002\mombot\ -dSolutionExt =.sln - dSolutionFileName = Mombot.sln - dSolutionName = Mombot - dSolutionPath = C:\Projects\TW2002\mombot\Mombot.sln - dConfiguration = Debug - dOutDir = ..\ -dPlatform = x86 - dProjectDir = C:\Projects\TW2002\mombot\source\Installer\ -dProjectExt =.wixproj - dProjectFileName = Installer.wixproj - dProjectName = Installer - dProjectPath = C:\Projects\TW2002\mombot\source\Installer\Installer.wixproj - dTargetDir = C:\Projects\TW2002\mombot\source\ -dTargetExt =.msi - dTargetFileName = Mombot5.0 - Pre1.msi - dTargetName = Mombot5.0 - Pre1 - dTargetPath = C:\Projects\TW2002\mombot\source\Mombot5.0 - Pre1.msi -out obj\Debug\ -arch x86 - ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixUtilExtension.dll" - ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixUIExtension.dll" Product.wxs
+     //   C:\Program Files(x86)\WiX Toolset v3.11\bin\Light.exe -out C:\Projects\TW2002\mombot\source\Mombot5.0 - Pre1.msi - pdbout C:\Projects\TW2002\mombot\source\Mombot5.0 - Pre1.wixpdb - cultures:null - ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixUtilExtension.dll" - ext "C:\Program Files (x86)\WiX Toolset v3.11\bin\\WixUIExtension.dll" - contentsfile obj\Debug\Installer.wixproj.BindContentsFileListnull.txt - outputsfile obj\Debug\Installer.wixproj.BindOutputsFileListnull.txt - builtoutputsfile obj\Debug\Installer.wixproj.BindBuiltOutputsFileListnull.txt - wixprojectfile C:\Projects\TW2002\mombot\source\Installer\Installer.wixproj obj\Debug\Product.wixobj
 
-
-
-        if (!File.Exists(path))
-        {
-            // Create a file to write to.
-        }
 
         //Console.ReadLine();
     }
+
+
 }
