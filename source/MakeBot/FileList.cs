f985011254deeea7ebd84a2d74de0e9ed13dc769
MicroBlaster <dmc73120@gmail.com>
2023-01-12 00:15:13 -0600

    Updated Installer

---
+++ source/MakeBot/FileList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace MakeBot
+{
+    public class FileList
+    {
+        public int DirIndex { get; private set; }
+        public int FileIndex { get; private set; }
+        public List<string> Files  { get; private set; }
+        public string Root { get; private set; }
+
+        //private StringBuilder fb;
+
+        public FileList(string root)
+        {
+            DirIndex = -1;
+            FileIndex = 0;
+           //Files = "";
+            Root = root;
+
+            //fb = new StringBuilder();
+            Files = new List<string>();
+
+
+            Enumerate();
+
+            //Files = fb.ToString();
+        }
+
+        private void Enumerate(String path = "")
+        {
+            Files.Add($"..{path}\\mombot.ts");
+
+            try
+            {
+                foreach (var d in Directory.GetDirectories($"{Root}\\{path}"))
+                {
+                    var dir = new DirectoryInfo(d);
+
+
+                    if (!((dir.Name == "MakeMSI") || (dir.Name == "MakeBot") || (dir.Name == "Installer") || (dir.Name == "bot_includes") || (dir.Name == "module_includes")))
+                    {
+                        DirIndex++;
+                        Console.WriteLine($"D{DirIndex:D4}:{path}\\{dir.Name}");
+
+
+                        foreach (var f in Directory.GetFiles($"{Root}\\{path}\\{dir.Name}"))
+                        {
+                            var file = new FileInfo(f);
+                            if ((file.Extension == ".ts") || (file.Extension == ".cts") && 
+                                !System.IO.File.Exists(file.FullName.Replace(".cts", ".ts")))
+                            {
+                                Files.Add($"..{path}\\{dir.Name}\\{file.Name}");
+                                FileIndex++;
+                            }
+                        }
+
+                        // Recursive call to inumerate sub directories
+                        Enumerate($"{path}\\{dir.Name}");
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- source/MakeBot/Main.xaml.cs
+++ source/MakeBot/Main.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,16 +21,100 @@ namespace MakeBot
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : Window
     {
-        public MainWindow()
+        public string Path { get; set; }
+        public FileList Scripts { get; set; }
+        public int Completed { get; set; }
+        //public List<Task> Tasks { get; set; }
+
+        public Main()
         {
             InitializeComponent();
+
+            //List<Task> tasks = new List<Task>();
+
+            Path = Environment.CurrentDirectory;
+            Path = Path.Replace(@"\bin\Debug", "");
+            Path = Path.Replace(@"\bin\Release", "");
+            Path = Path.Replace(@"\MakeBot", "");
+            //string file = $"{path}\\Installer\\Product.wxs";
+
+            Scripts = new FileList(Path);
+            ProgressBar.Maximum = Scripts.Files.Count();
+
+
+
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void onContentRendered(object sender, EventArgs e)
+        {
+            List<Task> tasks = new List<Task>();
+
+            ThreadPool.SetMaxThreads(100, 100);
+
+            foreach (var file in Scripts.Files)
+            {
+                tasks.Add(Task.Run(() => CompileScript(file)));
+            }
+
+            Task.Run(() => Task.WhenAll(tasks).Wait()).ContinueWith(t =>
+            Dispatcher.Invoke(() =>
+            {
+                ProgressLabel.Content = $"Completed 87 of 88 with 1 error.";
+                ErrorLabel.Visibility = Visibility.Visible;
+
+                Cancel.Visibility = Visibility.Hidden;
+                Finish.Visibility = Visibility.Visible;
+            }));
+
+
+
+        }
+
+        private void CompileScript(string file)
+        {
+            //Thread.Sleep(1000);
+
+            var f = new FileInfo(file);
+            var cts = new FileInfo(file.Replace(".ts", ".cts"));
+
+            if (f.Extension == ".ts")
+            {
+                if (cts.Exists) cts.Delete();
+
+                //Process.Start("twxc " + f.Name + " > compile.log");
+                Process.Start("twxc.exe", f.Name);
+            }
+
+
+
+            this.Dispatcher.Invoke(() =>
+            {
+                ProgressBar.Value = ++Completed;
+                ProgressLabel.Content = $"Compiled: {cts.Name}";
+            });
+
+
+        }
+
+        private void onCancelClick(object sender, RoutedEventArgs e)
         {
 
+            Close();
         }
+        private void onFinishClick(object sender, RoutedEventArgs e)
+        {
+
+            Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
     }
 }
