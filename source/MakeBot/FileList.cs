5ec416203497e0008b8e7b2301bc879578367bb0
MicroBlaster <dmc73120@gmail.com>
2023-01-13 03:48:33 -0600

    Updated Installer

--- source/MakeBot/FileList.cs
+++ source/MakeBot/FileList.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 using System.Xml.Linq;
 using static System.Net.WebRequestMethods;
 
@@ -11,8 +12,6 @@ namespace MakeBot
 {
     public class FileList
     {
-        public int DirIndex { get; private set; }
-        public int FileIndex { get; private set; }
         public List<string> Files  { get; private set; }
         public string Root { get; private set; }
 
@@ -20,23 +19,17 @@ namespace MakeBot
 
         public FileList(string root)
         {
-            DirIndex = -1;
-            FileIndex = 0;
-           //Files = "";
+
             Root = root;
 
-            //fb = new StringBuilder();
             Files = new List<string>();
-
+            Files.Add($"mombot.ts");
 
             Enumerate();
-
-            //Files = fb.ToString();
         }
 
         private void Enumerate(String path = "")
         {
-            Files.Add($"..{path}\\mombot.ts");
 
             try
             {
@@ -45,25 +38,26 @@ namespace MakeBot
                     var dir = new DirectoryInfo(d);
 
 
-                    if (!((dir.Name == "MakeMSI") || (dir.Name == "MakeBot") || (dir.Name == "Installer") || (dir.Name == "bot_includes") || (dir.Name == "module_includes")))
+                    if (!((dir.Name == "Source") || (dir.Name == "MakeMSI") || (dir.Name == "MakeBot") || (dir.Name == "Installer") || (dir.Name == "bot_includes") || (dir.Name == "module_includes")))
                     {
-                        DirIndex++;
-                        Console.WriteLine($"D{DirIndex:D4}:{path}\\{dir.Name}");
-
-
                         foreach (var f in Directory.GetFiles($"{Root}\\{path}\\{dir.Name}"))
                         {
                             var file = new FileInfo(f);
                             if ((file.Extension == ".ts") || (file.Extension == ".cts") && 
                                 !System.IO.File.Exists(file.FullName.Replace(".cts", ".ts")))
                             {
-                                Files.Add($"..{path}\\{dir.Name}\\{file.Name}");
-                                FileIndex++;
+                                if (path == "")
+                                    Files.Add($"{dir.Name}\\{file.Name}");
+                                else
+                                    Files.Add($"{path}\\{dir.Name}\\{file.Name}");
                             }
                         }
 
                         // Recursive call to inumerate sub directories
-                        Enumerate($"{path}\\{dir.Name}");
+                        if (path == "")
+                            Enumerate($"{dir.Name}");
+                        else
+                            Enumerate($"{path}\\{dir.Name}");
                     }
                 }
             }
--- source/MakeBot/Main.xaml.cs
+++ source/MakeBot/Main.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,6 +29,9 @@ namespace MakeBot
         public int Completed { get; set; }
         //public List<Task> Tasks { get; set; }
 
+        private StreamWriter clog, elog, wlog;
+        private int ccount, ecount, wcount;
+
         public Main()
         {
             InitializeComponent();
@@ -43,30 +47,63 @@ namespace MakeBot
             Scripts = new FileList(Path);
             ProgressBar.Maximum = Scripts.Files.Count();
 
+            if (File.Exists("Compile.log")) File.Delete("Compile.log");
+            if (File.Exists("Error.log")) File.Delete("Error.log");
+            if (File.Exists("Warning.log")) File.Delete("Warning.log");
 
-
+            clog = File.CreateText("Compile.log");
+            elog = File.CreateText("Error.log");
+            wlog = File.CreateText("Warning.log");
+            ccount = 0;
+            ecount = 0;
+            wcount = 0;
 
         }
 
         private void onContentRendered(object sender, EventArgs e)
         {
+            Stopwatch sw = new Stopwatch();
             List<Task> tasks = new List<Task>();
 
             ThreadPool.SetMaxThreads(100, 100);
 
+            clog.WriteLine($"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")} Biliding Mombot 5");
+
+            sw.Start();
+            //foreach (var file in Scripts.Files.Take(10))
             foreach (var file in Scripts.Files)
             {
+                var dst = new FileInfo("..\\" + file);
+                if (!(dst.Directory.Exists))
+                {
+                    Directory.CreateDirectory(dst.DirectoryName);
+                }
+
                 tasks.Add(Task.Run(() => CompileScript(file)));
             }
 
             Task.Run(() => Task.WhenAll(tasks).Wait()).ContinueWith(t =>
             Dispatcher.Invoke(() =>
             {
-                ProgressLabel.Content = $"Completed 87 of 88 with 1 error.";
-                ErrorLabel.Visibility = Visibility.Visible;
+                sw.Stop();
+                FinishedLabel.Content = $"Elapsed Time { sw.Elapsed}";
+
+                ProgressLabel.Content = $"Completed {ccount} of {Scripts.Files.Count()} with {ecount} error{(ecount==1?"":"s")}.";
+                if(ecount > 0)
+                    ErrorLabel.Visibility = Visibility.Visible;
+                else
+                    FinishedLabel.Visibility = Visibility.Visible;
+
 
                 Cancel.Visibility = Visibility.Hidden;
                 Finish.Visibility = Visibility.Visible;
+
+                clog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Finished Compiling.");
+                clog.Close();
+                elog.Close();
+                wlog.Close();
+                if (ecount == 0) File.Delete("Error.log");
+                if (wcount == 0) File.Delete("Warning.log");
             }));
 
 
@@ -75,20 +112,83 @@ namespace MakeBot
 
         private void CompileScript(string file)
         {
-            //Thread.Sleep(1000);
+            Stopwatch sw = new Stopwatch();
 
             var f = new FileInfo(file);
             var cts = new FileInfo(file.Replace(".ts", ".cts"));
+            var dst = new FileInfo("..\\" + file.Replace(".ts", ".cts"));
 
             if (f.Extension == ".ts")
             {
                 if (cts.Exists) cts.Delete();
 
-                //Process.Start("twxc " + f.Name + " > compile.log");
-                Process.Start("twxc.exe", f.Name);
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "twxc.exe",
+                        //WorkingDirectory = f.DirectoryName,
+                        Arguments = file,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                bool success = false;
+                //int size, lines, def, cmds;
+                int lines = 0;
+                string line = "";
+
+                sw.Start();
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    line = proc.StandardOutput.ReadLine();
+
+                    if (line == "Compilation successful.")
+                        success = true;
+                    //if (line.Contains("Code Size:"))
+                    //    size = Int32.Parse(line.Split(' ')[2]);
+                    if (line.Contains("Lines:"))
+                        lines = Int32.Parse(line.Split(' ')[1]);
+                    //if (line.Contains("Definitions:"))
+                    //    size = Int32.Parse(line.Split(' ')[1]);
+                    //if (line.Contains("Commands:"))
+                    //    size = Int32.Parse(line.Split(' ')[1]);
+                }
+                sw.Stop();
+
+                if (success == true)
+                {
+                    ccount++;
+                    clog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {cts.Name}: Compiled {lines} lines in {sw.Elapsed}");
+
+                    if (dst.Exists) dst.Delete();
+                    cts.MoveTo(dst.FullName);
+
+                }
+                else
+                {
+                    ecount++;
+                    clog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {cts.Name}: Failed to compile.");
+                    clog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {cts.Name}: {line}.");
+
+                    if (ecount == 1) elog.WriteLine($"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")} Biliding Mombot 5");
+                    elog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {cts.Name}: Failed to compile.");
+                    elog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {cts.Name}: {line}.");
+                }
             }
+            else
+            {
+                ccount++;
+                wcount++;
+                if (wcount == 1) wlog.WriteLine($"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")} Biliding Mombot 5");
+                wlog.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {cts.Name}: No source file found.");
 
-
+                if (dst.Exists) dst.Delete();
+                cts.CopyTo(dst.FullName);
+            }
 
             this.Dispatcher.Invoke(() =>
             {
