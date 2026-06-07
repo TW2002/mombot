110387243802f2809c4d2435ce4fd1e6dd22b915
mmosley <mosleym@gmail.com>
2026-06-07 01:34:41 -0400

    Build Mombot release with includes

--- mombot5.0/source/modes/resource/pimp.ts
+++ mombot5.0/source/modes/resource/pimp.ts
@@ -221,7 +221,6 @@ send $planet~planetnum "*"
 gosub :planet~getplanetinfo
 gosub :planet~updateplanetprods
 setvar $skip_prods_read 1
-end
 
 if ((($planet~planetfuel < $player~total_holds) or ($emptyfuel = false)) and (($planet~planetorg < $player~total_holds) or ($emptyorganics = false)) and (($planet~planetequip< $player~total_holds) or ($emptyequipment = false)))
 	#Blow it up :D
